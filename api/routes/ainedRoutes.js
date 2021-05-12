d4b998b7ce6e3bd489570ee0b71f919433e17095
Treksek <60392643+Treksek@users.noreply.github.com>
2021-05-12 20:40:40 +0300

    Neljas kodutöö

--- api/routes/ainedRoutes.js
+++ api/routes/ainedRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', ainedController.getAined);
 router.get('/:id', ainedController.getAineById);
-router.post('/', isLoggedIn, ainedController.createAine);
+router.post('/',  ainedController.createAine);
 router.patch('/:id', isLoggedIn, ainedController.changeAine);
 router.delete('/:id', isLoggedIn, isAdmin, ainedController.deleteAine);
 
